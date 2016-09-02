3bd3ac3a9cc80620dc45c4ca4eeb9ecd678a542f
Sean McGill <semcgill@nextgen.com>
2016-09-02 14:20:44 -0400

    removed un-needed junk in main

--- app/main.js
+++ app/main.js
@@ -1,10 +1,8 @@
 /// <reference path="../typings/index.d.ts" />
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-//import { platformBrowser } from '@angular/platform-browser';
 var core_1 = require('@angular/core');
 require('./rxjs-operators');
 core_1.enableProdMode();
 var app_module_1 = require('./app.module');
-//platformBrowser().bootstrapModuleFactory(AppModule); //TODO: determine how to get ahead of time compile working NgModuleFactory?
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,10 @@
 /// <reference path="../typings/index.d.ts" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-//import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import './rxjs-operators';
 
 enableProdMode();
 
 import { AppModule } from './app.module';
-
-//platformBrowser().bootstrapModuleFactory(AppModule); //TODO: determine how to get ahead of time compile working NgModuleFactory?
 platformBrowserDynamic().bootstrapModule(AppModule);
